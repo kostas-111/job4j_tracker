519d9b185a1d70388fd77bb89fdb151ec4ddf86e
kostas-111 <galkink370@gmail.com>
2024-02-04 08:32:46 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    boolean active;
+    int status;
+    String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Активность: " + active);
+        System.out.println("Номер ошибки: " + status);
+        System.out.println("Комментарий: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error defaultEr = new Error();
+        Error arithmeticEx = new Error(true, 1, "Попытка целочисленного деления на нуль");
+        Error emptyStack = new Error(false, 2, "Попытка извлечения объекта из пустого стека");
+        defaultEr.printInfo();
+        arithmeticEx.printInfo();
+        emptyStack.printInfo();
+    }
+}
