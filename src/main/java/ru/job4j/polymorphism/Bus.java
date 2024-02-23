9cf222b27caae05c646e5024985aa03194005967
kostas-111 <galkink370@gmail.com>
2024-02-23 09:50:27 +0300

    4.2. Интерфейс Input [#181092 #482581]

--- src/main/java/ru/job4j/polymorphism/Bus.java
+++ src/main/java/ru/job4j/polymorphism/Bus.java
@@ -3,16 +3,18 @@ package ru.job4j.polymorphism;
 public class Bus implements Transport {
     @Override
     public void go() {
-
+        System.out.println("Автобус начал движение.");
     }
 
     @Override
     public void passengers(int number) {
+        System.out.println("Количество пассажиров в автобусе: " + number);
 
     }
 
     @Override
     public double refuel(double fuelAmount) {
-        return 0;
+        double fuelPrice = 60.35;
+        return fuelAmount * fuelPrice;
     }
 }
