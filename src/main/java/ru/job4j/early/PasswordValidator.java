fde9d72c5b098a74dd2d42d41ca42bfd2ed59a35
kostas-111 <galkink370@gmail.com>
2024-03-11 00:13:22 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #485096]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -24,45 +24,39 @@ public class PasswordValidator {
         for (char symbol : password.toCharArray()) {
             if (Character.isUpperCase(symbol)) {
                 hasUpCase = true;
-                break;
+            }
+
+            if (Character.isLowerCase(symbol)) {
+                hasLowCase = true;
+            }
+
+            if (Character.isDigit(symbol)) {
+                hasDigit = true;
+            }
+
+            if (!Character.isLetterOrDigit(symbol)) {
+                hasSpecial = true;
             }
         }
+
         if (!hasUpCase) {
             throw new IllegalArgumentException(
                     "Password should contain at least one uppercase letter"
             );
         }
 
-        for (char symbol : password.toCharArray()) {
-            if (Character.isLowerCase(symbol)) {
-                hasLowCase = true;
-                break;
-            }
-        }
         if (!hasLowCase) {
             throw new IllegalArgumentException(
                     "Password should contain at least one lowercase letter"
             );
         }
 
-        for (char symbol : password.toCharArray()) {
-            if (Character.isDigit(symbol)) {
-                hasDigit = true;
-                break;
-            }
-        }
         if (!hasDigit) {
             throw new IllegalArgumentException(
                     "Password should contain at least one figure"
             );
         }
 
-        for (char symbol : password.toCharArray()) {
-            if (!Character.isLetterOrDigit(symbol)) {
-                hasSpecial = true;
-                break;
-            }
-        }
         if (!hasSpecial) {
             throw new IllegalArgumentException(
                     "Password should contain at least one special symbol"
