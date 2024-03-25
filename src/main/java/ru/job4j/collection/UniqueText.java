6cacc64d31a0b6c26e694c7ba876c9be26727ea0
kostas-111 <galkink370@gmail.com>
2024-03-25 23:31:49 +0300

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,23 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        for (String words : origin) {
+           check.add(words);
+        }
+        for (String words : text) {
+            if (!check.contains(words)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
