47d8c660036483c6c01f544439a36e0f4894ea0f
kostas-111 <galkink370@gmail.com>
2024-04-08 21:29:26 +0300

    0. Map, HashMap [#212652 #489924]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,9 @@ public class UsageMap {
         HashMap<String, String> myMap = new HashMap<>();
         myMap.put("galkin@rambler.ru", "Galkin Konstantin");
         myMap.put("jonny@mail.com", "Jonny B");
+        myMap.put("jonny@mail.com", "Jonny McGregor");
+        myMap.put("ron@gmail.com", "Ron Potter");
+        myMap.put("ron@gmail.com", "Ron Potter");
         for (String key : myMap.keySet()) {
             System.out.println(key + " = " + myMap.get(key));
         }
