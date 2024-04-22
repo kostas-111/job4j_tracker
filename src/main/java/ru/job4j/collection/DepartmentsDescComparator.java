721faed504185b316555c6b06d8b8a82c94db8c5
kostas-111 <galkink370@gmail.com>
2024-04-22 22:03:08 +0300

    3. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/DepartmentsDescComparator.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepartmentsDescComparator implements Comparator<String> {
+    @Override
+    public int compare(String left, String right) {
+        String[] l = left.split("/");
+        String[] r = right.split("/");
+        int result = r[0].compareTo(l[0]);
+        if (r[0].compareTo(l[0]) == 0) {
+            result = left.compareTo(right);
+        }
+        return result;
+    }
+}
