ee5911e1bc018c4918b860f22a39895a0f9ad8da
kostas-111 <galkink370@gmail.com>
2025-05-07 19:03:58 +0300

    fix: 1. Lombok. Настройка IDEA [#504888]
    - Исправлены все ошибки, возникавшие при запуске тестов

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,17 +1,16 @@
 package ru.job4j.tracker;
 
+import lombok.Data;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
+@Data
 public class Item {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
-
-    public Item() {
-    }
 
     public Item(String name) {
         this.name = name;
@@ -28,26 +27,9 @@ public class Item {
         this.created = created;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
+    /*
+    Переопределяем toString вручную, так как нужно кастомное форматирование даты
+     */
     @Override
     public String toString() {
         return "Item{"
@@ -57,6 +39,10 @@ public class Item {
                 + '}';
     }
 
+    /*
+    Переопределяем equals и hashCode вручную,
+    они не должны учитывать поле created
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
